c5dd5f5204f4003182c2504dd097632a154ef648
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-10-21 02:13:38 +0530

    implementing book ticket functionality

--- src/ticket.cpp
+++ src/ticket.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <fstream>
 #include <iomanip>
+#include <algorithm> // For std::transform
 
 #include "ticket.h"
 #include "bus.h"
@@ -37,3 +38,124 @@ void Ticket::displayTicket()
     cout << "\n";
 }
 
+// BOOK TICKET
+void Ticket::bookTicket()
+{
+    system("cls");
+
+    string from, to;
+    int chk = 0;
+
+    Bus b;
+    fstream busFileStream, ticketFileStream, tempFileStream;
+
+    printHeading("BOOK TICKET");
+
+    busFileStream.open("buses.dat", ios::in | ios::app | ios::binary);
+    if (!busFileStream)
+    {
+        cout << "\n\t\t\t\t\t\t\t\t\t\tCan't Open File...!!\n";
+    }
+
+    else
+    {
+        cout << "\n\t\t\t\t\t\t\t\t\t\tEnter Source:-> ";
+        cin.ignore();
+        getline(cin,from);
+        cout << "\n\t\t\t\t\t\t\t\t\t\tEnter Destination:-> ";
+        getline(cin,to);
+
+        system("cls");
+        printHeading("AVAILABLE BUSES");
+
+        busFileStream.read((char *)&b, sizeof(b));
+        while (!busFileStream.eof())
+        {
+             string user_from = b.getSource();
+            string user_to = b.getDestination();
+
+    // Convert both strings to lowercase
+    transform(user_from.begin(), user_from.end(), user_from.begin(), ::tolower);
+    transform(user_to.begin(), user_to.end(), user_to.begin(), ::tolower);
+
+            if ((user_from == from) && (user_to == to) )
+            {
+                b.showBusDetails();
+                chk = 1;
+            }
+            busFileStream.read((char *)&b, sizeof(b));
+        }
+
+        busFileStream.close();
+
+        if (chk == 0)
+        {
+            cout << "\n\t\t\t\t\t\t\t\t\t\tNo Buses Found...!!\n";
+        }
+        else
+        {
+            string bNo;
+            int booked = 0;
+
+            cout << "\n\t\t\t\t\t\t\t\t\t\tEnter Bus Number:-> ";
+            getline(cin,bNo);
+
+            busFileStream.open("buses.dat", ios::in | ios::app | ios::binary);
+            tempFileStream.open("temp.dat", ios::out | ios::app | ios::binary);
+
+            busFileStream.read((char *)&b, sizeof(b));
+            while (!busFileStream.eof())
+            {
+                string user_from = b.getSource();
+            string user_to = b.getDestination();
+
+    // Convert both strings to lowercase
+    transform(user_from.begin(), user_from.end(), user_from.begin(), ::tolower);
+    transform(user_to.begin(), user_to.end(), user_to.begin(), ::tolower);
+
+                if ((user_from == from) && (user_to == to) && (b.getBusNo() == bNo) )
+                {
+                    if (b.getBookedSeats() >= 32)
+                    {
+                        cout << "\n\t\t\t\t\t\t\t\t\t\tSeat not available...!!\n";
+                        break;
+                    }
+                    else
+                    {
+                        system("cls");
+                        printHeading("BOOK TICKET");
+                        string n;
+                        cout << "\n\t\t\t\t\t\t\t\t\t\tEnter Customer Name :-> ";
+                        getline(cin,n);
+                        b.setBookedSeats();
+                        generateTicket(n, b);
+                        ticketFileStream.open("tickets.dat", ios::out | ios::app | ios::binary);
+                        ticketFileStream.write((char *)this, sizeof(*this));
+                        ticketFileStream.close();
+                        tempFileStream.write((char *)&b, sizeof(b));
+
+                        booked = 1;
+                        system("cls");
+                        printHeading("BOOKING DETAILS");
+                        displayTicket();
+                        cout << "\n\t\t\t\t\t\t\t\t\t\tTicket Booked Successfully...!!\n";
+                    }
+                }
+                else
+                {
+                    tempFileStream.write((char *)&b, sizeof(b));
+                }
+                busFileStream.read((char *)&b, sizeof(b));
+            }
+
+            if (booked == 1)
+            {
+                busFileStream.close();
+                tempFileStream.close();
+                remove("buses.dat");
+                rename("temp.dat", "buses.dat");
+            }
+        }
+        busFileStream.close();
+    }
+}
